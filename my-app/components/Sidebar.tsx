21af147de238831b7996f484e7c5e714459760a4
hcodesk <lhzik20@gmail.com>
2025-01-28 17:36:48 +0000

    new changes

--- my-app/components/Sidebar.tsx
+++ my-app/components/Sidebar.tsx
@@ -1,6 +1,14 @@
+import { useRouter } from 'next/router'
 import React from 'react'
+import { useNavigation } from '@/lib/context/navigation'
 
 export default function Sidebar() {
+   const router = useRouter() //hook pour manipuler la navigation 
+   const { closeMobileNav } = useNavigation()
+
+
+
+
   return (
     <div>Sidebar</div>
   )
